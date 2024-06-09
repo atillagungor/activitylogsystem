25b4de44224403dcfab081ad033ed48f6fc604f0
Atilla Güngör <atillaagungorr@gmail.com>
2024-06-09 15:45:41 +0300

    base api added

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,13 +1,3 @@
 let BASE_API_URL: string;
-
-const isWindows = () => {
-    return navigator.userAgent.indexOf("Win") !== -1;
-}
-
-if (isWindows()) {
-    BASE_API_URL = "http://localhost:38106/api/";  
-} else {
-    BASE_API_URL = "http://localhost:5110/api/";  
-}
-
+    BASE_API_URL = "https://localhost:44386/api/";
 export { BASE_API_URL };
